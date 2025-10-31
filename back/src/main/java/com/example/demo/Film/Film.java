f10c5cc7f54dc9317d1f534f52cda93356586443
carlosdelrioperez <80700839+carlosdelrioperez@users.noreply.github.com>
2025-10-31 17:30:22 +0100

    first commit

---
+++ back/src/main/java/com/example/demo/Film/Film.java
@@ -0,0 +1,8 @@
+package com.example.demo.Film;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Film {
+
+}
---
+++ back/src/main/java/com/example/demo/Film/FilmRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.Film;
+
+public class FilmRepository {
+
+}
